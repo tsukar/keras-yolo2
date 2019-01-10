346e98c062a189148b3aef563e680c5394ca3497
Ryotaro Tsukada <tsukada@iba.t.u-tokyo.ac.jp>
2019-01-10 23:47:24 +0900

    Remove some operations of image augmentation

--- preprocessing.py
+++ preprocessing.py
@@ -101,12 +101,12 @@ class BatchGenerator(Sequence):
                 iaa.SomeOf((0, 5),
                     [
                         #sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
-                        iaa.OneOf([
-                            iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
-                            iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-                            iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
-                        ]),
-                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+                        #iaa.OneOf([
+                        #    iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                        #    iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+                        #    iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                        #]),
+                        #iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
                         #iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
                         # search either for all edges or for directed edges
                         #sometimes(iaa.OneOf([
@@ -114,10 +114,10 @@ class BatchGenerator(Sequence):
                         #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
                         #])),
                         iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
-                        iaa.OneOf([
-                            iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
-                            #iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-                        ]),
+                        #iaa.OneOf([
+                        #    iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+                        #    #iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                        #]),
                         #iaa.Invert(0.05, per_channel=True), # invert color channels
                         iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
                         iaa.Multiply((0.5, 1.5), per_channel=0.5), # change brightness of images (50-150% of original value)
